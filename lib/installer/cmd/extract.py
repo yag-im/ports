699a96446641cb5501c52bde97c9305db7405aa0
Robert Ayrapetyan <robert@yag.im>
2025-02-18 05:53:31 +0000

    add: gabriel knight 2

--- lib/installer/cmd/extract.py
+++ lib/installer/cmd/extract.py
@@ -8,12 +8,15 @@ from lib.unpack import (
     unpack_disc_image,
 )
 
+HINT_FLOPPY = "floppy"
+
 
 def run(task: dict, app_descr: AppDesc) -> None:
     src = Path(task.get("src"))
     files = task.get("files")
     dest = Path(task.get("dest"))
     cd_images_as_letters = task.get("cd_images_as_letters", False)
+    hint = task.get("hint", None)
     if cd_images_as_letters:
         unpack_cd_images_as_letters(
             src_dir=src,
@@ -22,9 +25,10 @@ def run(task: dict, app_descr: AppDesc) -> None:
             first_cd_letter=task.get("first_cd_drive_letter", FIRST_CD_DRIVE_LETTER),
         )
     else:
-        if "cd" in app_descr.distro.format.lower():
-            # src.suffix.lower()[1:] in ["iso", "nrg", "mdf", "pdi", "cdi", "bin", "cue", "b5i", "img"]:
-            # FD images can also have img extensions, so switch by distro format
+        if hint == HINT_FLOPPY:
+            # floppy images can have "img" extensions (like CDs), so using a hint here
+            unpack_archive(src, dest, files)
+        elif src.suffix.lower()[1:] in ["iso", "nrg", "mdf", "pdi", "cdi", "bin", "cue", "b5i", "img"]:
             unpack_disc_image(src, dest, files)
         else:
             unpack_archive(src, dest, files)
