dc689f8f5a6d175f783681ff4bff74ec7c497165
Robert Ayrapetyan <robert@yag.im>
2026-01-15 03:12:32 +0000

    add: Math for the Real World

--- runners/dosbox/win98/install.sh
+++ runners/dosbox/win98/install.sh
@@ -3,21 +3,42 @@
 
 # https://dosbox-x.com/wiki/Guide:Installing-Windows-98
 
-cd $DATA_DIR/runners/src/win98
+RUNNER_SRC=$DATA_DIR/runners/src/win9x
+OS_FLAVOR=win98se
+LANG=""
+
+if [[ $# -lt 1 ]]; then
+  echo "Usage: $0 [en|ru|ja]"
+  exit 1
+fi
+
+case "$1" in
+  en|ru|ja)
+    LANG="$1"
+    ;;
+  *)
+    echo "Usage: $0 [en|ru|ja]"
+    exit 1
+    ;;
+esac
+
+cd $DATA_DIR/runners/src/win9x/win98se-$LANG/dosbox-x
 
 # bootstrap
 
 dosbox-x -conf win98-bootstrap.conf
-IMGMAKE C -t hd -size 512
 
-IMGMOUNT C C
-IMGMOUNT D win98se.iso
-IMGMOUNT A -bootcd D
-BOOT A:
+# Type manually in dosbox prompt:
+
+# IMGMAKE C -t hd -size 512
+# IMGMOUNT C C
+# IMGMOUNT D ../win98se.iso
+# IMGMOUNT A -bootcd D
+# BOOT A:
 
 # after reboot - continue installation
 
-dosbox-x -c "IMGMOUNT D win98se.iso" -conf win98se.conf
+dosbox-x -c "IMGMOUNT D ../win98se.iso" -conf win98se.conf
 
 # key VMGGK-72FPD-2PHRP-3HV4R-FYJQJ
 
